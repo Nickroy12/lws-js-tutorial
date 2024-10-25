4eccefd8f6d02fb567565f60308b0519ea8e2315
Nick roy <nroy08232@gmail.com>
2024-10-25 17:38:57 +0600

    DOM HTML

---
+++ #90 DOM HTML/script.js
@@ -0,0 +1,7 @@
+function clickButton(){
+  addEventListener("click", function(){
+     document.getElementById('demo').innerHTML = "Songita Roy"
+  })
+}
+
+document.getElementById("image").src = "https://e7.pngegg.com/pngimages/922/441/png-clipart-creative-bulb-lightbulb-energy-saving-lamps-thumbnail.png";
