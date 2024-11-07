bb3fe5ac0fa3087fd7d501f7d1c32f06c5969843
Nick roy <nroy08232@gmail.com>
2024-11-07 13:16:32 +0600

    Add navigation

---
+++ #105 BOM Navigation/script.js
@@ -0,0 +1,9 @@
+document.getElementById("demo").innerHTML = navigator.platform;
+document.getElementById("demo2").innerHTML = navigator.appName;
+document.getElementById("demo3").innerHTML = navigator.appCodeName;
+document.getElementById("demo4").innerHTML = navigator.cookieEnabled;
+document.getElementById("demo5").innerHTML = navigator.userAgent;
+document.getElementById("demo6").innerHTML = navigator.language;
+document.getElementById("demo7").innerHTML = navigator.product;
+document.getElementById("demo8").innerHTML = navigator.online;
+document.getElementById("demo9").innerHTML = navigator.javaEnabled();
