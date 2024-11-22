539d90b030cbf12c0c0f4ddab528870bde03dc6a
Nick-roy <nroy08232@gmail.com>
2024-11-22 22:43:35 +0600

    add folder

---
+++ #122 lws json/script.js
@@ -0,0 +1,7 @@
+const userId = {
+    name : "Nick roy",
+    age: 21,
+    country : "Bangladesh"
+}
+
+console.log(userId)
---
+++ #123 lws json syntex/script.js
@@ -0,0 +1,12 @@
+const userId = {
+    name : "Nick roy",
+    age: 21,
+    country : "Bangladesh",
+    book: {
+       name : "nai"
+    },
+    alive:true,
+    wife:null
+}
+
+console.log(JSON.stringify(userId))
